31dfb70240637515b322094cba9c94eecd67a8e3
andrescaro16 <julicaro2003@gmail.com>
2023-04-28 20:51:52 -0500

    Menu fusion on mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 let email = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
+let hambMobileMenu = document.querySelector(".menu");
+let mobileMenu = document.querySelector(".mobile-menu");
 
 email.addEventListener("click", toggleDesktopMenu);
+hambMobileMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
