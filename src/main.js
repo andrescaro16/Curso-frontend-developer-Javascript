2babb194cc209273c7d7ca118a6e81bd185f643a
andrescaro16 <julicaro2003@gmail.com>
2023-04-28 22:13:39 -0500

    Shopping cart aside

--- src/main.js
+++ src/main.js
@@ -2,14 +2,34 @@ let email = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 let hambMobileMenu = document.querySelector(".menu");
 let mobileMenu = document.querySelector(".mobile-menu");
+let cartIcon = document.querySelector(".navbar-shopping-cart");
+let cart = document.querySelector(".product-detail");
+
 
 email.addEventListener("click", toggleDesktopMenu);
 hambMobileMenu.addEventListener("click", toggleMobileMenu);
+cartIcon.addEventListener("click", toggleCart);
+
 
 function toggleDesktopMenu(){
+    if(!cart.classList.contains("inactive")){
+        cart.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    if(!cart.classList.contains("inactive")){
+        cart.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCart(){
+    if(!desktopMenu.classList.contains("inactive")){
+        desktopMenu.classList.toggle("inactive");
+    }else if(!mobileMenu.classList.contains("inactive")){
+        mobileMenu.classList.toggle("inactive");
+    }
+    cart.classList.toggle("inactive");
 }
